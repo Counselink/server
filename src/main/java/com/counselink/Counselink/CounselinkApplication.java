2c482b63beaa5b1839cf1ba31cc732bafea1bdb3
comeeasy <zz1236zz@naver.com>
2022-07-05 23:43:51 +0900

    first commit

---
+++ src/main/java/com/counselink/Counselink/CounselinkApplication.java
@@ -0,0 +1,13 @@
+package com.counselink.Counselink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CounselinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CounselinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/counselink/Counselink/CounselinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.counselink.Counselink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CounselinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
