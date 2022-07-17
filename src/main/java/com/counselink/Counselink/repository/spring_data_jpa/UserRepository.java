0f936b45ae6fe2f9bd71ec245e929bfefb006461
comeeasy <zz1236zz@naver.com>
2022-07-17 19:23:54 +0900

    Feat: login_id 로 user를 검색하는 함수 선언

--- src/main/java/com/counselink/Counselink/repository/spring_data_jpa/UserRepository.java
+++ src/main/java/com/counselink/Counselink/repository/spring_data_jpa/UserRepository.java
@@ -3,5 +3,8 @@ package com.counselink.Counselink.repository.spring_data_jpa;
 import com.counselink.Counselink.entity.member.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByLoginId(String loginId);
 }
