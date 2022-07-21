966046349bbb6b58692a00648d7dde512ea602f5
yejun_1220@naver.com <yejun_1220@naver.com>
2022-07-21 09:31:03 +0900

    Feat: UserReviewRepository 추가

---
+++ src/main/java/com/counselink/Counselink/repository/UserReviewRepository.java
@@ -0,0 +1,7 @@
+package com.counselink.Counselink.repository;
+
+import com.counselink.Counselink.entity.UserReview;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserReviewRepository extends JpaRepository<UserReview, Long> {
+}
