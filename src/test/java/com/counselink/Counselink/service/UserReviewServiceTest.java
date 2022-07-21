88c277513b31d3fa73cc9d1635618c721c454a22
yejun_1220@naver.com <yejun_1220@naver.com>
2022-07-21 10:24:12 +0900

    Test: 회원가입, 예약하기, 리뷰작성 테스트 코드 작성

---
+++ src/test/java/com/counselink/Counselink/service/UserReviewServiceTest.java
@@ -0,0 +1,57 @@
+package com.counselink.Counselink.service;
+
+import com.counselink.Counselink.entity.Address;
+import com.counselink.Counselink.entity.CounselInformation;
+import com.counselink.Counselink.entity.member.Counselor;
+import com.counselink.Counselink.entity.member.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootTest
+@Transactional
+@Rollback(value = false)
+class UserReviewServiceTest {
+
+    @PersistenceContext EntityManager em;
+    @Autowired private ReserveService reserveService;
+    @Autowired private UserReviewService userReviewService;
+
+    @Test
+    public void 리뷰작성() {
+        // 유저
+        Address address = Address.builder().city("city").street("street").zipcode("zipcode").build();
+        User user = new User("name", "number", "email", "id", "password", address);
+
+        em.persist(user);
+
+        // 카운셀러
+        Counselor counselor = Counselor.builder().build();
+        em.persist(counselor);
+
+        // 예약할 정보
+        CounselInformation counselInformation1 = CounselInformation.createCounselInformation(
+                "category",  LocalDateTime.of(2022, 7, 15, 14,0), LocalDateTime.of(2022, 7, 15, 14,0), 10000, counselor);
+
+        CounselInformation counselInformation2 = CounselInformation.createCounselInformation(
+                "category", LocalDateTime.of(2022, 7, 15, 14,0), LocalDateTime.of(2022, 7, 15, 14,0), 10000, counselor);
+
+        em.persist(counselInformation1);
+        em.persist(counselInformation2);
+
+        List<Long> counselInformationList = new ArrayList<>();
+        counselInformationList.add(counselInformation1.getId());
+        counselInformationList.add(counselInformation2.getId());
+
+        long reserveId = reserveService.reserve(user.getId(), counselInformationList);
+        userReviewService.write(user.getId(), reserveId, "상담을 잘해요.", 5);
+    }
+}
