703bf669d714ebed065d29c1158702fb0ee6d86d
yejun_1220@naver.com <yejun_1220@naver.com>
2022-07-21 09:29:10 +0900

    Revert "Feat: UserReviewRepository 추가"
    This reverts commit 5d0a4e4ff8cfb67f25dbf831186399ce5b3bf47b.

--- src/main/java/com/counselink/Counselink/repository/UserReviewRepository.java
+++
@@ -1,7 +0,0 @@
-package com.counselink.Counselink.repository;
-
-import com.counselink.Counselink.entity.UserReview;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserReviewRepository extends JpaRepository<UserReview, Long> {
-}
