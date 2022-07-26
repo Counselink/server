424c3db2aeb8b5e1551b7815a21821ceadc75d96
comeeasy <zz1236zz@naver.com>
2022-07-26 14:44:42 +0900

    Feat: Splited login into userLogin and counselorLogin

--- src/main/java/com/counselink/Counselink/login/CounselorService.java
+++ src/main/java/com/counselink/Counselink/login/CounselorService.java
@@ -1,2 +1,23 @@
-package com.counselink.Counselink.login;public class CounselorService {
+package com.counselink.Counselink.login;
+
+import com.counselink.Counselink.entity.member.Counselor;
+import com.counselink.Counselink.repository.spring_data_jpa.CounselorRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class CounselorService {
+
+    private final CounselorRepository counselorRepository;
+
+    @Transactional
+    public void isLoginValid(Counselor counselor) throws Exception {
+        Optional<Counselor> counselors = counselorRepository.findByEmail(counselor);
+        if (!counselors.isPresent()) {throw new Exception("login is not valid");}
+    }
 }
--- src/main/java/com/counselink/Counselink/login/LoginController.java
+++ src/main/java/com/counselink/Counselink/login/LoginController.java
@@ -1,5 +1,6 @@
 package com.counselink.Counselink.login;
 
+import com.counselink.Counselink.entity.member.Counselor;
 import com.counselink.Counselink.entity.member.User;
 import com.counselink.Counselink.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -19,13 +20,23 @@ import javax.transaction.Transactional;
 public class LoginController {
 
     private final UserService userService;
+    private final CounselorService counselorService;
 
-    // 로그인
-    @PostMapping(path = "/login")
-    public ResponseEntity<?> login(
+    // User Login
+    @PostMapping(path = "/userLogin")
+    public ResponseEntity<?> userLogin(
         @RequestBody User user
     ) {
         userService.isLoginValid(user);
+        return ResponseEntity.ok().body(null);
+    }
+
+    // Counselor Login
+    @PostMapping(path = "/userLogin")
+    public ResponseEntity<?> counselorLogin(
+            @RequestBody Counselor counselor
+    ) throws Exception {
+        counselorService.isLoginValid(counselor);
         return ResponseEntity.ok().body(null);
     }
 }
