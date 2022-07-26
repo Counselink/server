2d93a8c76857b29e96dbd99a0781231610116fb0
comeeasy <zz1236zz@naver.com>
2022-07-26 13:51:08 +0900

    Feat: Defined Global Exception handler class

--- src/main/java/com/counselink/Counselink/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/counselink/Counselink/common/exception/GlobalExceptionHandler.java
@@ -1,2 +1,27 @@
-package com.counselink.Counselink.common.exception;public class GlobalExceptionHandler {
+package com.counselink.Counselink.common.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.text.ParseException;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ParseException.class)
+    public ResponseEntity<String> handleParseException(ParseException e) {
+        log.error(e.toString());
+        return ResponseEntity.badRequest().body("parse exception");
+    }
+
+    @ExceptionHandler(InvalidLoginException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidLoginException(InvalidLoginException e) {
+        log.error(e.toString());
+        ErrorResponse response = new ErrorResponse(ErrorCode.USER_NOT_FOUND);
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
 }
