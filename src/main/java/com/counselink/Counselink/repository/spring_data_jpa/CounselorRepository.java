5c63bbf20d34563856473b7f2d2181d57bcc64af
comeeasy <zz1236zz@naver.com>
2022-07-26 14:42:46 +0900

    Feat: Defined query state which finds counselor records matched by email

--- src/main/java/com/counselink/Counselink/repository/spring_data_jpa/CounselorRepository.java
+++ src/main/java/com/counselink/Counselink/repository/spring_data_jpa/CounselorRepository.java
@@ -3,5 +3,8 @@ package com.counselink.Counselink.repository.spring_data_jpa;
 import com.counselink.Counselink.entity.member.Counselor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CounselorRepository extends JpaRepository<Counselor, Long> {
+    Optional<Counselor> findByEmail(Counselor counselor);
 }
