88809c8266fa06526a99b58cf3c13506aafa3174
comeeasy <zz1236zz@naver.com>
2022-07-30 14:52:05 +0900

    # feat: add swagger configuration
    <domain>/swagger-ui.html 로 접속 가능

---
+++ src/main/java/com/counselink/Counselink/api/scheduling/SchedulingController.java
@@ -0,0 +1,14 @@
+package com.counselink.Counselink.api.scheduling;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping(value = "/api/scheduling")
+@RequiredArgsConstructor
+public class SchedulingController {
+
+}
