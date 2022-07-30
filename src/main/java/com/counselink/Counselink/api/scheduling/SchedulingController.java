b71d84c0b271bf4fe116bed58f0309376b9afc0c
comeeasy <zz1236zz@naver.com>
2022-07-30 15:18:20 +0900

    # test: define test counselink informatino reserve api

--- src/main/java/com/counselink/Counselink/api/scheduling/SchedulingController.java
+++ src/main/java/com/counselink/Counselink/api/scheduling/SchedulingController.java
@@ -1,14 +1,29 @@
 package com.counselink.Counselink.api.scheduling;
 
+import com.counselink.Counselink.entity.CounselInformation;
+import com.counselink.Counselink.service.ReserveService;
+import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
 @Controller
 @RequestMapping(value = "/api/scheduling")
 @RequiredArgsConstructor
+@Api(tags = {"일정"})
 public class SchedulingController {
 
+    private final ReserveService reserveService;
+
+    @PostMapping(path = "/register")
+    public ResponseEntity<String> register(@RequestBody CounselInformation ci) {
+        reserveService.saveCounselInfo(ci);
+
+        return ResponseEntity.ok().body(ci.toString());
+    }
 }
